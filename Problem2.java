c980b21a8cd90d9ad81b217930b62dd246c68945
sdumbre1 <sdumbre@charlotte.edu>
2024-07-01 15:07:36 +0530

    Done Competitive Coding-2

---
+++ Problem2.java
@@ -0,0 +1,28 @@
+class Problem2{
+
+    private static int problem2(int W, int weight[],int profit[],int n){
+
+        int[][] dp = new int[n + 1][W + 1];
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j <= W; j++) {
+                if (i == 0 || j == 0) {
+                    dp[i][j] = 0;
+                } else if (weight[i - 1] <= j) {
+                    dp[i][j] = Math.max(profit[i - 1] + dp[i - 1][j - weight[i - 1]], dp[i - 1][j]);
+                } else {
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+
+        return dp[n][W];
+    }
+    public static void main(String args[])
+    {
+        int profit[] = new int[] { 60, 100, 120 };
+        int weight[] = new int[] { 10, 20, 30 };
+        int W = 50;
+        int n = profit.length;
+        System.out.println(problem2(W, weight, profit, n));
+    }
+}
